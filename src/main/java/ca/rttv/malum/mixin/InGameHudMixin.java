05c719b2026172041077317460717e86b6853bd6
Arathain <arathainfarqoe@gmail.com>
2022-04-19 20:33:12 +0200

    I am speed

--- src/main/java/ca/rttv/malum/mixin/InGameHudMixin.java
+++ src/main/java/ca/rttv/malum/mixin/InGameHudMixin.java
@@ -1,6 +1,8 @@
 package ca.rttv.malum.mixin;
 
 import ca.rttv.malum.util.handler.ScreenParticleHandler;
+import ca.rttv.malum.util.spirit.spiritaffinity.ArcaneAffinity;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,6 +15,11 @@ public abstract class InGameHudMixin {
     @Inject(at = @At("HEAD"), method = "renderHotbar")
     private void malum$renderHotbarStart(float l1, MatrixStack j1, CallbackInfo ci) {
         ScreenParticleHandler.renderingHotbar = false;
+    }
+    @Inject(method = "renderStatusBars(Lnet/minecraft/client/util/math/MatrixStack;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;pop()V"))
+    private void renderArmorOverlay(MatrixStack matrices, CallbackInfo info) {
+        ArcaneAffinity.ClientOnly.renderSoulWard(matrices, MinecraftClient.getInstance().getWindow());
     }
     @Inject(at = @At("RETURN"), method = "renderHotbar")
     private void malum$renderHotbarEnd(float l1, MatrixStack j1, CallbackInfo ci) {
