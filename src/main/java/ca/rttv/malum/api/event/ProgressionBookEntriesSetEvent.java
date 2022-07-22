8cd0ff2c789702201d990f1b98a298426d6fe9e1
mrsterner <simon@sterner.dev>
2022-07-23 00:02:01 +0200

    comment

--- src/main/java/ca/rttv/malum/api/event/ProgressionBookEntriesSetEvent.java
+++ src/main/java/ca/rttv/malum/api/event/ProgressionBookEntriesSetEvent.java
@@ -7,7 +7,12 @@ import java.util.ArrayList;
 
 import static net.fabricmc.fabric.api.event.EventFactory.createArrayBacked;
 
-
+/**For adding more entries to the book
+ * ProgressionBookEntriesSet.EVENT.register(arrayList -> {
+ * 			arrayList.add(new BookEntry("test", Items.ACACIA_SAPLING, 9,8)
+ * 					.addPage(new HeadlineTextPage("Test", "testText")));
+ *                });
+ */
 public interface ProgressionBookEntriesSetEvent {
     Event<ProgressionBookEntriesSetEvent> EVENT = createArrayBacked(ProgressionBookEntriesSetEvent.class, listeners -> (entries) -> {
         for (ProgressionBookEntriesSetEvent listener : listeners) {
