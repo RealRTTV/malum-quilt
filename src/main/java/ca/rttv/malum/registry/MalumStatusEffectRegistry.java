e43f931d47ec006178e9bb5d299d2417c838a6e7
Mephody <40513410+Mephodio@users.noreply.github.com>
2023-05-18 19:34:49 +0300

    Fix aqueous rite effect

--- src/main/java/ca/rttv/malum/registry/MalumStatusEffectRegistry.java
+++ src/main/java/ca/rttv/malum/registry/MalumStatusEffectRegistry.java
@@ -1,6 +1,7 @@
 package ca.rttv.malum.registry;
 
 import ca.rttv.malum.util.spirit.SpiritType;
+import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
@@ -18,7 +19,7 @@ public interface MalumStatusEffectRegistry {
 
     StatusEffect AERIAL_AURA            = register("aerial_aura",            new StatusEffect(StatusEffectType.BENEFICIAL, SpiritType.AERIAL_SPIRIT.color.getRGB()).addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, "E3F9C028-D6CC-4CF2-86A6-D5B5EFD86BE6", 0.2d, EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
     StatusEffect CORRUPTED_AERIAL_AURA  = register("corrupted_aerial_aura",  new StatusEffect(StatusEffectType.BENEFICIAL, SpiritType.AERIAL_SPIRIT.color.getRGB()));
-    StatusEffect AQUEOUS_AURA           = register("aqueous_aura",           new StatusEffect(StatusEffectType.BENEFICIAL, SpiritType.AQUEOUS_SPIRIT.color.getRGB()));
+    StatusEffect AQUEOUS_AURA           = register("aqueous_aura",           new StatusEffect(StatusEffectType.BENEFICIAL, SpiritType.AQUEOUS_SPIRIT.color.getRGB()).addAttributeModifier(ReachEntityAttributes.REACH, "a2a69bb7-468d-4c45-a004-eae2f4471e5e", 2.0d, EntityAttributeModifier.Operation.ADDITION).addAttributeModifier(ReachEntityAttributes.ATTACK_RANGE, "0669c96f-32b8-4e26-8385-64ca857ee687", 2.0d, EntityAttributeModifier.Operation.ADDITION));
     StatusEffect SACRED_AURA            = register("sacred_aura",            new StatusEffect(StatusEffectType.BENEFICIAL, SpiritType.SACRED_SPIRIT.color.getRGB()));
     StatusEffect EARTHEN_AURA           = register("earthen_aura",           new StatusEffect(StatusEffectType.BENEFICIAL, SpiritType.EARTHEN_SPIRIT.color.getRGB()).addAttributeModifier(EntityAttributes.GENERIC_ARMOR, "04448cbf-ee2c-4f36-b71f-e641a312834a", 3.0d, EntityAttributeModifier.Operation.MULTIPLY_TOTAL).addAttributeModifier(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, "dc5fc5d7-db54-403f-810d-a16de6293ffd", 1.5d, EntityAttributeModifier.Operation.ADDITION));
     StatusEffect CORRUPTED_EARTHEN_AURA = register("corrupted_earthen_aura", new StatusEffect(StatusEffectType.BENEFICIAL, SpiritType.EARTHEN_SPIRIT.color.getRGB()).addAttributeModifier(EntityAttributes.GENERIC_ATTACK_DAMAGE, "e2a25284-a8b1-41a5-9472-90cc83793d44", 2.0d, EntityAttributeModifier.Operation.ADDITION));
